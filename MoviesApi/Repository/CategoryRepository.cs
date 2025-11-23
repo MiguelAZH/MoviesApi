55db3ab22db5ac3c30c51702ada0c0f904fa513c
migue <migue@MIGUELLITO>
2025-11-23 13:11:06 -0500

    new CategoryRepository and Interface created

---
+++ MoviesApi/Repository/CategoryRepository.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApi.DAL;
+using MoviesApi.DAL.Models;
+using MoviesApi.Repository.IRepository;
+
+namespace MoviesApi.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CategoryExistByNameAsync(string name)
+        {
+            return await _context.Categories
+                .AsNoTracking()
+                .AnyAsync(c => c.Name == name);
+        }
+
+        public async Task<bool> CategoryExistsByIdAsync(int id)
+        {
+            return await _context.Categories
+                .AsNoTracking()
+                .AnyAsync(c => c.Id == id);
+        }
+
+        public async Task<bool> CreateCategoryAsync(Category category)
+        {
+            category.CreatedDate = DateTime.UtcNow;
+            await _context.AddAsync(category);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeleteCategoryAsync(int id)
+        {
+            var category = await GetCategoryByIdAsync(id);
+
+           if (category == null)
+            {
+                return false; //category not found
+            }
+            _context.Categories.Remove(category); //removing category
+            return await SaveAsync();
+        }
+
+        public async Task<ICollection<Category>> GetCategoriesAsync()
+        {
+            return await _context.Categories
+                .AsNoTracking()
+                .OrderBy(c=> c.Name) //ordering by Name
+                .ToListAsync();
+        }
+
+        public async Task<Category?> GetCategoryByIdAsync(int id) //async and await 
+        {
+            return await _context.Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id); //lambda expression 
+ 
+        }
+
+        public async Task<bool> UpdateCategoryAsync(Category category)
+        {
+            category.ModifiedDate = DateTime.UtcNow;
+            var addcategory = _context.Update(category);
+            return await SaveAsync();
+        }
+
+        private async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() >= 0 ? true : false; //saving changes to database or SQL INSERT
+        }
+    }
+}
---
+++ MoviesApi/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using MoviesApi.DAL.Models;
+
+namespace MoviesApi.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>> GetCategoriesAsync(); // Get all categories
+        Task<Category?> GetCategoryByIdAsync(int id); // Get category by ID
+        Task<bool> CategoryExistsByIdAsync(int id); // Check if category exists fot ID
+        Task<bool> CategoryExistByNameAsync(string name); // Check if category exists for name
+        Task<bool> CreateCategoryAsync(Category category); // Create a new category
+        Task<bool> UpdateCategoryAsync(Category category); //  Update existing category (Name and UpdateDate)
+        Task<bool> DeleteCategoryAsync(int id); // Delete a category
+    }
+}
